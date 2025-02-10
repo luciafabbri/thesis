00ade5864a1911514078cde476fdc39e4b22168b
luciafabbri <luciafabbri1999@gmail.com>
2025-02-10 23:14:52 +0100

    .

--- app.py
+++ app.py
@@ -41,15 +41,79 @@ def load_questions():
         questions.extend(random_questions)
     return questions  
 
-# Salvataggio risposte nel database (modifica essenziale)
+# Salvataggio risposte nel database
 def save_responses(age_group, gender, questions, responses):
     conn = get_db_connection()
     cur = conn.cursor()
-    for question, response in zip(questions, responses):
-        cur.execute(
-            "INSERT INTO responses (age_group, gender, question, response) VALUES (%s, %s, %s, %s)",
-            (age_group, gender, question, response)
-        )
+
+    # Aggiungi la query per l'inserimento dei dati nelle colonne corrette
+    query = """
+        INSERT INTO responses (age_group, gender, 
+                               q1_text, q1_response, 
+                               q2_text, q2_response, 
+                               q3_text, q3_response, 
+                               q4_text, q4_response, 
+                               q5_text, q5_response, 
+                               q6_text, q6_response, 
+                               q7_text, q7_response, 
+                               q8_text, q8_response, 
+                               q9_text, q9_response, 
+                               q10_text, q10_response, 
+                               q11_text, q11_response, 
+                               q12_text, q12_response, 
+                               q13_text, q13_response, 
+                               q14_text, q14_response, 
+                               q15_text, q15_response, 
+                               q16_text, q16_response, 
+                               q17_text, q17_response, 
+                               q18_text, q18_response, 
+                               q19_text, q19_response, 
+                               q20_text, q20_response, 
+                               q21_text, q21_response, 
+                               q22_text, q22_response, 
+                               q23_text, q23_response, 
+                               q24_text, q24_response, 
+                               q25_text, q25_response)
+        VALUES (%s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s, 
+                %s, %s)
+    """
+    
+    # Crea una lista con le risposte da inserire
+    values = [age_group, gender]
+    
+    # Assumiamo che le risposte siano una lista con le risposte per ogni domanda
+    for i in range(len(questions)):
+        question_column = f"q{i+1}_text"
+        response_column = f"q{i+1}_response"
+        question = questions[i]
+        response = responses[i]
+        
+        values.extend([question, response])
+    
+    # Esegui l'inserimento nel database
+    cur.execute(query, values)
     conn.commit()
     cur.close()
     conn.close()
@@ -66,6 +130,7 @@ def start_survey():
 def survey():
     if request.method == "POST":
         questions = session.get("questions", [])
+        # Recupera le risposte
         responses = [request.form.get(f"q{i}") for i in range(1, len(questions) + 1)]
         age_group = session.get("age_group")
         gender = session.get("gender")
